3e34b440d7ebeee54d989262433caf519009aac8
Brian Salomone <brian.j.salomone@gmail.com>
2023-06-20 19:56:45 -0700

    header column aligned

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const projectCards = document.querySelector(".projects")
+projectCards.style.border = "2px solid green"
 
 for (let i=0; i<6; i++) {
     const cardElement = document.createElement("div")
